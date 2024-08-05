e1b38fe52d937ba4c5e14daa572463f3b9924594
Kayla Moorcroft <kayla.moorcroft@icloud.com>
2024-08-05 20:02:22 +0000

    feat: display message for new user and add to database
    feat: display message for new user
    
    feat: add new user to database

--- number_guess.sh
+++ number_guess.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=number_guess -t -q --no-align -c"
 
 echo "Enter your username:"
 read USERNAME
+
+EXISTING_USERNAME=$($PSQL "SELECT username FROM users WHERE username = '$USERNAME'")
+
+# if username has not been used before
+if [[ -z $EXISTING_USERNAME ]]
+then
+  # print welcome message
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  # add username to database
+  $PSQL "INSERT INTO users(username) VALUES('$USERNAME')"
+fi
